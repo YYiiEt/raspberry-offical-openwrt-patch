890848eea5309201bf863e7892419650cf87c7e6
YYiiEt <60560292+YYiiEt@users.noreply.github.com>
2020-02-27 17:08:44 +0800

    Add files via upload

--- repair.sh
+++ repair.sh
@@ -3,10 +3,11 @@
 wget https://raw.githubusercontent.com/YYiiEt/raspberry-offical-openwrt-patch/master/model.tar.gz
 tar -zxvf model.tar.gz
 if [ -d "model/" ];then
-  cd model
-  rm -f /usr/libexec/rpcd/luci && cp luci /usr/libexec/rpcd/
-  rm -f /usr/share/rpcd/acl.d/luci-base.json && cp luci-base.json /usr/share/rpcd/acl.d/
-  rm -f /www/luci-static/resources/view/status/include/10_system.js && cp 10_system.js /www/luci-static/resources/view/status/include/
-  /etc/init.d/rpcd restart
+	cd model
+	rm -f /usr/libexec/rpcd/luci && cp luci /usr/libexec/rpcd/
+	rm -f /usr/share/rpcd/acl.d/luci-base.json && cp luci-base.json /usr/share/rpcd/acl.d/
+	rm -f /www/luci-static/resources/view/status/include/10_system.js && cp 10_system.js /www/luci-static/resources/view/status/include/
+	/etc/init.d/rpcd restart
+	cd .. && rm -rf model model.tar.gz repair.sh
+	echo "ok"
 fi
-rm -rf model model.tar
