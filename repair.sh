b6eb4f332b26e7beed8586f0fb2fac01b306c323
YYiiEt <60560292+YYiiEt@users.noreply.github.com>
2020-02-27 17:09:34 +0800

    Update repair.sh

--- repair.sh
+++ repair.sh
@@ -5,7 +5,9 @@ tar -zxvf model.tar.gz
 if [ -d "model/" ];then
 	cd model
 	rm -f /usr/libexec/rpcd/luci && cp luci /usr/libexec/rpcd/
+	/etc/init.d/rpcd restart
 	rm -f /usr/share/rpcd/acl.d/luci-base.json && cp luci-base.json /usr/share/rpcd/acl.d/
+	/etc/init.d/rpcd restart
 	rm -f /www/luci-static/resources/view/status/include/10_system.js && cp 10_system.js /www/luci-static/resources/view/status/include/
 	/etc/init.d/rpcd restart
 	cd .. && rm -rf model model.tar.gz repair.sh
